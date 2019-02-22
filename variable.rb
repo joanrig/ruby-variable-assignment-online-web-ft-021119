221d76c13e7bbe89d1ab62210ad37951ec2c7c5a
Joan Lyness <joanrigdon@gmail.com>
2019-02-22 06:29:39 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,3 +1,20 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-greeting = "Hello World"
+#greeting = "Hello World"
+
+matrix = [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+
+def reverse_on_diagonals(matrix)
+  matrix.map {
+  a = matrix.first.first
+  b = matrix.last.last
+
+  c = matrix.first.last
+  d = matrix.last.first
+
+  a,b = b,a
+  c,d = d,c
+  binding.pry
+end
